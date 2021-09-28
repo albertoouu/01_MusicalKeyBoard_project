cf0cdc13a0eb49ed285f2c5ec58f465d9e17d524
albertoouu <albertoouu@gmail.com>
2021-09-28 11:09:54 -0500

    arreglo iniciacion arreglo

--- app.js
+++ app.js
@@ -1,10 +1,2 @@
-const keys = document.querySelectorAll('.key');
-
-keys.forEach(key => {
-  key.addEventListener('click', () => playNote(key))
-})
-
-function playNote(key) {
-  const noteAudio = document.getElementById(key.dataset.note)
-  noteAudio.play()
-  }
+const keys = document.querySelectorAll('.key')
+console.log(keys);
